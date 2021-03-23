d67b5a23cda3e7220ff0bf0d2eae45ed44cca5ef
cindy <expectative06@gmail.com>
2021-03-23 17:26:04 +0800

    Add ColorSlider.swift

---
+++ RedBullEye/ColorSlider.swift
@@ -0,0 +1,24 @@
+//
+//  ColorSlider.swift
+//  RedBullEye
+//
+//  Created by  cindy on 2021/3/23.
+//
+
+import SwiftUI
+
+struct ColorSlider: View {
+    
+    @Binding var value: Double
+   var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+                .foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255")
+                .foregroundColor(textColor)
+        }.padding(.horizontal)
+    }
+}
