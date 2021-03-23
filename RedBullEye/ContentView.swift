08d75fa0bd6053e4c5c03e32910c4d2b2d59e345
cindy <expectative06@gmail.com>
2021-03-23 17:26:17 +0800

    Add three colorsliders to contentView

--- RedBullEye/ContentView.swift
+++ RedBullEye/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color!")
+                        .padding()
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R:\(Int(rGuess * 255)) G:\(Int(gGuess * 255)) B:\(Int(bGuess * 255))")
+                        .padding()
+                }
+            }
+            Button(action: {}) {
+                Text("Hit me!")
+            }
+            
+            ColorSlider(value: $rGuess, textColor: .red )
+            ColorSlider(value: $gGuess, textColor: .green )
+            ColorSlider(value: $bGuess, textColor: .blue )
+        }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+            // Landscape
+            .previewLayout(.fixed(width: 568, height: 320))
     }
-}
+}   
