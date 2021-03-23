310ee1dd9bad29b3c2b5b1d68d616bb03c47646b
cindy <expectative06@gmail.com>
2021-03-23 16:48:41 +0800

    Add SwiftUI project

---
+++ RedBullEye/RedBullEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedBullEyeApp.swift
+//  RedBullEye
+//
+//  Created by  cindy on 2021/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RedBullEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
