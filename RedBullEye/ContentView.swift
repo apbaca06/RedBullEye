45dd4d0c2aa5772f54db1bf38115f3a76279563c
cindy <expectative06@gmail.com>
2021-03-23 18:30:05 +0800

    Add navigationView and dark mode for contentView

--- RedBullEye/ContentView.swift
+++ RedBullEye/ContentView.swift
@@ -18,31 +18,38 @@ struct ContentView: View {
     @State var showAlert = false
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack {
-                    Color(red: rTarget, green: gTarget, blue: bTarget)
-                    Text("Match this color!")
-                        .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    VStack {
+                        Color(red: rTarget, green: gTarget, blue: bTarget)
+                        Text("Match this color!")
+                            .padding()
+                    }
+                    VStack {
+                        Color(red: rGuess, green: gGuess, blue: bGuess)
+                        Text("R:\(Int(rGuess * 255)) G:\(Int(gGuess * 255)) B:\(Int(bGuess * 255))")
+                            .padding()
+                    }
                 }
+                Button(action: {
+                    showAlert = true 
+                }) {
+                    Text("Hit me!")
+                }.alert(isPresented: $showAlert, content: {
+                    Alert(title: Text("Your Score"), message: Text(String(computeScore())), dismissButton: nil)
+                }).padding()
+                
                 VStack {
-                    Color(red: rGuess, green: gGuess, blue: bGuess)
-                    Text("R:\(Int(rGuess * 255)) G:\(Int(gGuess * 255)) B:\(Int(bGuess * 255))")
-                        .padding()
+                    ColorSlider(value: $rGuess, textColor: .red )
+                    ColorSlider(value: $gGuess, textColor: .green )
+                    ColorSlider(value: $bGuess, textColor: .blue )
                 }
             }
-            Button(action: {
-                showAlert = true
-            }) {
-                Text("Hit me!")
-            }.alert(isPresented: $showAlert, content: {
-                Alert(title: Text("Your Score"), message: Text(String(computeScore())), dismissButton: nil)
-            }).padding()
-            
-            ColorSlider(value: $rGuess, textColor: .red )
-            ColorSlider(value: $gGuess, textColor: .green )
-            ColorSlider(value: $bGuess, textColor: .blue )
         }
+        .navigationBarTitle("")
+        .environment(\.colorScheme, .dark)
+        .navigationBarHidden(true)
     }
     
     func computeScore() -> Int {
